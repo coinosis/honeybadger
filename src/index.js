59ddd7b63a6f46dff31a8759ea490673a0d978ff
Emilio Silva Schlenker <e18r@disroot.org>
2020-07-31 10:47:18 -0500

    user specifies list of addresses. working version

--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
 const abi = require('../contracts/ProxyEvent.abi.json');
 const Web3 = require('web3');
-const addresses = require('./addresses.js');
 
 const web3 = new Web3(Web3.givenProvider);
-const address = document.getElementById('contract');
-const amount = document.getElementById('attendees');
-let registeredTotal = [];
+const proxyInfo = document.getElementById('proxyInfo');
+const contractField = document.getElementById('contractField');
+const attendeesField = document.getElementById('attendeesField');
 
-window.register = async () => {
+window.connect = async () => {
   const accounts = await web3.eth.requestAccounts();
-  const contract = new web3.eth.Contract(abi, address.value);
+  proxyInfo.innerHTML = accounts[0];
+}
+
+window.registerFor = async () => {
+  if (!proxyInfo.innerHTML) {
+    alert('connect first');
+    return;
+  }
+  const contract = new web3.eth.Contract(abi, contractField.value);
+  const accounts = await web3.eth.requestAccounts();
   const fee = await contract.methods.fee().call();
   const attendees = await contract.methods.getAttendees().call();
   const registered = [];
-  for (let i = 0; i < 50; i++) {
-    if (!attendees.includes(web3.utils.toChecksumAddress(addresses[i]))) {
-      contract.methods.registerFor(addresses[i])
-        .send({from: accounts[0], value: fee});
-      registered.push(addresses[i]);
-      if (registered.length >= amount.value) break;
+  const newAttendees = attendeesField.value.split('\n');
+  for (let i = 0; i < newAttendees.length; i++) {
+    const newAttendee = web3.utils.toChecksumAddress(newAttendees[i]);
+    if (!attendees.includes(newAttendee)) {
+      contract.methods.registerFor(newAttendee)
+        .send({from: proxyInfo.innerHTML, value: fee});
     }
   }
-  registeredTotal = [...registeredTotal, ...registered];
 }
 
-window.vote = async () => {
-  const accounts = await web3.eth.requestAccounts();
-  const contract = new web3.eth.Contract(abi, address.value);
+window.clapFor = async () => {
+  if (!proxyInfo.innerHTML) {
+    alert('connect first');
+    return;
+  }
+  const contract = new web3.eth.Contract(abi, contractField.value);
   const attendees = await contract.methods.getAttendees().call();
-  const claps = attendees.map(a =>
-    Math.floor(Math.random() * 170 / attendees.length)
-  );
-  contract.methods.clapFor(registeredTotal[0], attendees, claps)
-    .send({from: accounts[0]});
+  const clappers = attendeesField.value.split('\n');
+  for (const clapper of clappers) {
+    const claps = attendees.map(a =>
+      Math.floor(Math.random() * 170 / attendees.length)
+    );
+    contract.methods.clapFor(clapper, attendees, claps)
+      .send({from: proxyInfo.innerHTML});
+  }
 }
